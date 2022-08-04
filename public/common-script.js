6e1a79d156d7f187d3039bf9356feeb46f7a6fc9
Ashwin A <ashwin.anil8104@gmail.com>
2022-08-04 20:05:27 +0530

    4-8-22 | 2

--- public/common-script.js
+++ public/common-script.js
@@ -11,3 +11,17 @@ window.onresize = function () {
         document.getElementById("nav_brand").classList.add("ms-3");
     }
 }
+
+if (window.location.href.endsWith("signUp")) {
+    document.getElementById("main-nav-link").textContent = "LOG IN";
+    document.getElementById("main-nav-link").href = "/login";
+    document.getElementById("custom-style-sheet").setAttribute("href", "css/signup-styles.css");
+
+} else if (window.location.href.endsWith("login")) {
+    document.getElementById("main-nav-link").textContent = "SIGN UP";
+    document.getElementById("main-nav-link").href = "/signUp";
+    document.getElementById("custom-style-sheet").setAttribute("href", "css/login-styles.css");
+} else { 
+    document.getElementById("main-nav-link").textContent = "LOG OUT";
+    document.getElementById("main-nav-link").href = "/logout";
+}
